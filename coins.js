68dcdd3d6361787abd4e653c8adf327466c56948
asimonia <alex.simonian@gmail.com>
2016-10-12 11:04:21 -0500

    Exercise progress

--- coins.js
+++ coins.js
@@ -1,4 +1,4 @@
-var startingAmount = 0.10;
+var startingAmount = .46;
 
 function coinCounter (amount) {
   // Initialize a JavaScript object to hold the coins
@@ -6,20 +6,31 @@ function coinCounter (amount) {
   var amountLeft = amount;
   var coinsNeeded = 0;
 
-  coinsNeeded = Math.floor(amountLeft / .25);
+  coinsNeeded = Math.floor(amountLeft / 0.25);
   coinPurse.quarters = coinsNeeded;
-  amountLeft -= coinPurse.quarters * .25;
+  if (!(amountLeft % .25)) {
+    return coinPurse;
+  }
+  amountLeft -= coinPurse.quarters * 0.25;
 
   coinsNeeded = Math.floor(amountLeft / .10);
   coinPurse.dimes = coinsNeeded;
+  if (!(amountLeft % .10)) {
+    return coinPurse;
+  }
   amountLeft -= coinPurse.dimes * .10;
 
   coinsNeeded = Math.floor(amountLeft / .05);
   coinPurse.nickels = coinsNeeded;
+  if (!(amountLeft % .05)) {
+    return coinPurse;
+  }
   amountLeft -= coinPurse.nickels * .05;
 
-  coinsNeeded = Math.floor(amountLeft / .01);
   coinPurse.pennies = coinsNeeded;
+  if (!(amountLeft % .01)) {
+    return coinPurse;
+  }
   amountLeft -= coinPurse.pennies * .01;
 
   return coinPurse;
